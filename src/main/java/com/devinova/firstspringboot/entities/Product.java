72baa7a536323900b615d734bb6f3e5d9785288f
heliopkuhn <heliopkuhn@gmail.com>
2021-04-17 23:09:42 -0300

    configuration db h2 and entities mapped

--- src/main/java/com/devinova/firstspringboot/entities/Product.java
+++ src/main/java/com/devinova/firstspringboot/entities/Product.java
@@ -2,12 +2,26 @@ package com.devinova.firstspringboot.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class Product implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy  = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private Double price;
+	
+	@ManyToOne
+	@JoinColumn(name = "category_id")
 	private Category category;
 	
 	
